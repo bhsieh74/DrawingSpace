3c18e063468698dcd9bb6b87e9bdbf50dfc3bd13
bhsieh74 <58496626+bhsieh74@users.noreply.github.com>
2020-05-02 18:18:44 -0400

    Completed 3 Drawings + Homepage
    Time for finishing touches. Will commit again soon

--- js/moving.js
+++ js/moving.js
@@ -1,3 +1,5 @@
+
+
 let comethorizontal = -150;
 let cometvertical = 10;
 
@@ -7,13 +9,19 @@ let ufovertical = 1;
 let satellitehorizontal = -1000;
 let satellitevertical = 100;
 
-let astronauthorizontal = -700;
+let astronauthorizontal = -100;
 let astronautvertical = 100;
 let astronautrotate = 0;
+let astronautdownwards = true;
+let astronautleft = true;
 
-let asteroidhorizontal = -1500;
+let asteroidhorizontal = 500;
 let asteroidvertical = 100;
+let asteroidrotate = 0;
 
+
+let spaceshiphorizontal = -300;
+let spaceshipvertical = 100;
 
 let animation = requestAnimationFrame(float);
 
@@ -43,26 +51,70 @@ animation = requestAnimationFrame(float);
     if (satellitehorizontal < 2000) {
         satellitehorizontal += 3; 
         objects[2].style.left = satellitehorizontal + 'px'; 
+        objects[2].style.transform = "rotateZ(" + asteroidrotate + "deg)";
+        asteroidrotate = asteroidrotate + .28;
     } 
     else {
         satellitehorizontal = -1600;
     }
 
-    if (astronauthorizontal < 2000) {
-        astronauthorizontal += 2; 
+    if (astronautleft == true) {
+        astronauthorizontal += 5; 
         objects[3].style.right = astronauthorizontal + 'px';
         objects[3].style.transform = "rotateZ(" + astronautrotate + "deg)";
-        astronautrotate = astronautrotate + .5;
-    } 
-    else {
-        astronauthorizontal = -1900;
+        astronautrotate = astronautrotate + 1;
+        if (astronauthorizontal == screen.width-250) {
+            astronautleft = false;
+        }
+    }
+
+    if (astronautleft == false) {
+        astronauthorizontal = astronauthorizontal - 5;
+        objects[3].style.right = astronauthorizontal + 'px';
+        objects[3].style.transform = "rotateZ(" + astronautrotate + "deg)";
+        astronautrotate = astronautrotate + 1;
+        if (astronauthorizontal == -100) {
+            astronautleft = true;
+        }
+    }
+
+    if (astronautdownwards == true) {
+        astronautvertical += 2;
+        objects[3].style.top = astronautvertical + 'px';
+        if (astronautvertical == 800) {
+            astronautdownwards = false;
+        }
+    }
+    if (astronautdownwards == false) {
+        astronautvertical -= 2;
+        objects[3].style.top = astronautvertical + 'px';
+        if (astronautvertical == 0) {
+            astronautdownwards = true;
+        }
     }
 
     if (asteroidhorizontal < 2000) {
         asteroidhorizontal += 1; 
-        objects[4].style.left = asteroidhorizontal + 'px'; 
+        objects[4].style.left = asteroidhorizontal + 'px';
+        objects[4].style.transform = "rotateZ(" + asteroidrotate + "deg)";
+        asteroidrotate = asteroidrotate + .28;
     }
     else {
         asteroidhorizontal = -2500;
     }
+
+
+    if (spaceshiphorizontal < 2000) {
+        spaceshiphorizontal += .5;
+        objects[5].style.left = spaceshiphorizontal + 'px';
+    }
+    else {
+        spaceshiphorizontal = -2000;
+    }
+
+    if (spaceshipvertical < 1000) {
+        spaceshipvertical += .2;
+        objects[5].style.top = spaceshipvertical + 'px';
+    }
+ 
 }   
